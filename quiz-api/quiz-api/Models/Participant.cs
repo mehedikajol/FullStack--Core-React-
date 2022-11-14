2e4c54201d20c06a77499cf195f70c9603fe63ce
Mehedi Hasan Kajol <mehedi.kajol.ice@gmail.com>
2022-11-14 22:45:40 +0600

    added models

---
+++ quiz-api/quiz-api/Models/Participant.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace quiz_api.Models
+{
+    public class Participant
+    {
+        public int ParticipantId { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string Email { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string Name { get; set; }
+        public int Score { get; set; }
+        public int TimeTaken { get; set; }
+    }
+}
---
+++ quiz-api/quiz-api/Models/Question.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace quiz_api.Models
+{
+    public class Question
+    {
+        public int QuestionId { get; set; }
+
+        [Column(TypeName = "nvarchar(250)")]
+        public string QuestionInWords { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string IamgeName { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string Option01 { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string Option02 { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string Option03 { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string Option04 { get; set; }
+        public int Answer { get; set; }
+    }
+}
