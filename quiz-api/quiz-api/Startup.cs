3b8c9cf1a36a9f93ac05bf069f182d9c6d46ba53
Mehedi Hasan Kajol <mehedi.kajol.ice@gmail.com>
2022-11-14 22:58:34 +0600

    added dbcontext and migration

--- quiz-api/quiz-api/Startup.cs
+++ quiz-api/quiz-api/Startup.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using quiz_api.Contexts;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,6 +27,8 @@ namespace quiz_api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<QuizDbContext>(options => 
+                options.UseSqlServer(Configuration.GetConnectionString("QuizConnection")));
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
