ea474d761b26761b2d255498dae4a9db7bff09fc
Mehedi Hasan Kajol <mehedi.kajol.ice@gmail.com>
2022-11-17 20:04:55 +0600

    created conetxt api

--- quiz-client/src/components/Question.js
+++ quiz-client/src/components/Question.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useContext } from "react";
+import { stateContext } from "../hooks/useStateContext";
 
 export default function Question() {
   return <div>Question</div>;
